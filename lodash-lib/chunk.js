b62aa34af937806b3c4ea328c48173bdf7040e31
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-05-05 00:28:14 +0600

    lodash created

---
+++ lodash-lib/chunk.js
@@ -0,0 +1,22 @@
+const chunk = (arr, num) => {
+  const temp = [...arr];
+  const numberOfArray = Math.ceil(arr.length / num);
+  let result = [];
+  for (let i = 0; i < numberOfArray; i++) {
+    result.push([]);
+  }
+  //console.log(result);
+  let idx = 0;
+  for (let i = 0; i < temp.length; i += num) {
+    let temp1 = i + num;
+    for (let j = i; j < temp1; j++) {
+      if (temp[j]) {
+        result[idx].push(temp[j]);
+      }
+    }
+    idx++;
+  }
+  //console.log(temp);
+  return result;
+};
+console.log(chunk([1, 2, 3, 4, 5, 6, 7], 2));
